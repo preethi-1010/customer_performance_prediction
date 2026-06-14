b8bc01caaea339821e5ebbbdf5c80e99e5352cd7
preethi <preethim1211@gmail.com>
2026-06-14 20:19:45 +0530

    Run database initialization on startup

--- app.py
+++ app.py
@@ -1790,6 +1790,9 @@ def init_db():
         print("    Visit http://127.0.0.1:5000/register")
         print("    First user to register = admin.")
 
+with app.app_context():
+    init()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
